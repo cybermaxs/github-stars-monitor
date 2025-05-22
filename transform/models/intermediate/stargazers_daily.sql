2e4242bfb8a4f5a3f39ffa6d1f015cbbeb1b6ed7
Maxime Lemaitre <maxime.lemaitre@gmail.com>
2025-05-22 15:20:51 +0200

    fix(transform): join stars to repositories so renamed repositories are handled correctly

--- transform/models/intermediate/stargazers_daily.sql
+++ transform/models/intermediate/stargazers_daily.sql
@@ -3,4 +3,5 @@ select
     , date_trunc('day', starred_at) as day
     , count(distinct user_login) as stars
 from {{ ref('stg_stargazers') }}
+inner join {{ ref('stg_repositories') }} using (repository)
 group by repository, day
